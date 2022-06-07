da094a88a6576a23a23727f2448d3bbb70c0fec2
Luiz Farias <luizferreirafariasn@gmail.com>
2022-06-06 21:51:05 -0300

    Input event target value is now saving in a state and i learned to break a line in a text component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 import * as C from './App.style';
 
 const App = () => {
-  const teste = "teste"
+  const [binData, setBinData] = useState();
   return (
     <C.Container>
       <C.Title>Convert binary to decimal</C.Title>
-      <C.Input />
+      <C.Input onChange={e => setBinData(e.target.value)}/>
       <C.Button>Convert</C.Button>
-      <C.Result>The decimal is: {teste}</C.Result>
+      <C.Result>
+        The decimal is: <br />{binData}
+      </C.Result>
     </C.Container>
   )
 }
--- src/App.style.js
+++ src/App.style.js
@@ -11,6 +11,15 @@ export const Title = styled.h1``;
 
 export const Input = styled.input``;
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  margin-top: 20px;
+`;
 
-export const Result = styled.div``;
+export const Result = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+`;
