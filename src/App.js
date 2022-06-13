c293008bf6ddc620f9093926f4f7aebc75c87d9f
Luiz Farias <luizferreirafariasn@gmail.com>
2022-06-13 02:42:01 -0300

    verify if the input number is a binary

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import { useState } from 'react';
 import * as C from './App.style';
 
 const App = () => {
-  const [binData, setBinData] = useState();
+  const [binData, setBinData] = useState('');
+
+  function handleBinData(binData) {
+    const binSpread = [...binData];
+    for (let i = 0; i<binSpread.length; i++){
+      if (binSpread[i] === '1' ||binSpread[i] === '0' ){
+        console.log('É binário');
+      }else {
+        alert('Por favor informe um número binário (0 ou 1)')
+        break;
+      } 
+    }
+    
+  }
+  
   return (
     <C.Container>
       <C.Title>Convert binary to decimal</C.Title>
       <C.Input onChange={e => setBinData(e.target.value)}/>
-      <C.Button>Convert</C.Button>
+      <C.Button onClick={() => handleBinData(binData)}>Convert</C.Button>
       <C.Result>
-        The decimal is: <br />{binData}
+        The decimal is: <br />
       </C.Result>
     </C.Container>
   )
