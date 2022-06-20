ddebc3a98637ad04f670d45e2a29501c2d32a483
Luiz Farias <luizferreirafariasn@gmail.com>
2022-06-20 04:23:09 -0300

    algorithm thats transform a binary to decimal and application to the button, app is working as it was planned

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import * as C from './App.style';
 
 const App = () => {
   const [binData, setBinData] = useState('');
+  const [decData, setDecData] = useState('teste');
 
   function handleBinData(binData) {
     const binSpread = [...binData];
+    let n = 1;
+    let bin2dec = 0;
     for (let i = 0; i<binSpread.length; i++){
       if (binSpread[i] === '1' ||binSpread[i] === '0' ){
-        console.log('É binário');
+        //Math.pow(2, binSpread.length - n)
+        bin2dec += (binSpread[i]*(Math.pow(2, (binSpread.length - n))));
+        n++;
+        setDecData(bin2dec);
       }else {
-        alert('Por favor informe um número binário (0 ou 1)')
+        alert('Por favor informe um número binário (0 ou 1)');
         break;
       } 
+      ;
     }
     
   }
@@ -23,7 +30,7 @@ const App = () => {
       <C.Input onChange={e => setBinData(e.target.value)}/>
       <C.Button onClick={() => handleBinData(binData)}>Convert</C.Button>
       <C.Result>
-        The decimal is: <br />
+        The decimal is: <br /> {decData}
       </C.Result>
     </C.Container>
   )
